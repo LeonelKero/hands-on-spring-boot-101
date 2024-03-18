096b739b08bfd2ca4e93a2e41e42d3c553258659
leonel ka <waboleonel@gmail.com>
2024-03-18 22:43:01 +0100

    Define customer entity using jpa

--- src/main/java/com/wbt/handsonspringboot101/customer/Customer.java
+++ src/main/java/com/wbt/handsonspringboot101/customer/Customer.java
@@ -1,11 +1,23 @@
 package com.wbt.handsonspringboot101.customer;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
 public class Customer {
+    @Id
+    @SequenceGenerator(
+            name = "customer_id_sequence",
+            allocationSize = 1,
+            sequenceName = "customer_id_sequence")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_id_sequence")
     private Long id;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String email;
+    @Column(nullable = false)
     private Integer age;
 
     public Customer() {
