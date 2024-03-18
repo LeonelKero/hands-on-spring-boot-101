7d58b0d4a016976ffab19fa18443b65e0e9c3555
leonel ka <waboleonel@gmail.com>
2024-03-18 02:39:11 +0100

    Initial commit

---
+++ src/main/java/com/wbt/handsonspringboot101/HandsOnSpringBoot101Application.java
@@ -0,0 +1,13 @@
+package com.wbt.handsonspringboot101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandsOnSpringBoot101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandsOnSpringBoot101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/wbt/handsonspringboot101/HandsOnSpringBoot101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wbt.handsonspringboot101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandsOnSpringBoot101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
