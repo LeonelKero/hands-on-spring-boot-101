91265502f52fd387ff6d028044ea60191b3bc1ad
leonel ka <waboleonel@gmail.com>
2024-03-20 01:39:01 +0100

    Setting up testcontainers for postgresql

--- src/test/java/com/wbt/handsonspringboot101/TestContainersTest.java
+++ src/test/java/com/wbt/handsonspringboot101/TestContainersTest.java
@@ -1,14 +1,24 @@
 package com.wbt.handsonspringboot101;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Testcontainers
 public class TestContainersTest {
+    @Container
+    private static final PostgreSQLContainer<?> postgresalContainer =
+            new PostgreSQLContainer<>("postgres:16")
+                    .withDatabaseName("wbt-dao-unit-test")
+                    .withUsername("postgres")
+                    .withPassword("postgres");
+
     @Test
     void canStartPostgresDB() {
-        // GIVEN
-
-        // WHEN
-
-        // THEN
+        assertThat(postgresalContainer.isRunning()).isTrue();
+        assertThat(postgresalContainer.isCreated()).isTrue();
     }
 }
