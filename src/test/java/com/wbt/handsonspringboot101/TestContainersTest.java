ef5efe602995475a3a4712d68aa6b1583b765676
leonel ka <waboleonel@gmail.com>
2024-03-20 01:17:18 +0100

    Add testcontainers, junit-jupiter, and postgresql library for testing

---
+++ src/test/java/com/wbt/handsonspringboot101/TestContainersTest.java
@@ -0,0 +1,14 @@
+package com.wbt.handsonspringboot101;
+
+import org.junit.jupiter.api.Test;
+
+public class TestContainersTest {
+    @Test
+    void canStartPostgresDB() {
+        // GIVEN
+
+        // WHEN
+
+        // THEN
+    }
+}
