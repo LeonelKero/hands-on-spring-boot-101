07771b4f63a4a32cbff78813224aff9b997d6a07
leonel ka <waboleonel@gmail.com>
2024-03-23 22:52:36 +0100

    Add javafaker api to help in testing

--- src/test/java/com/wbt/handsonspringboot101/journey/CustomerIntegrationTest.java
+++ src/test/java/com/wbt/handsonspringboot101/journey/CustomerIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.wbt.handsonspringboot101.journey;
 
+import com.github.javafaker.Faker;
 import com.wbt.handsonspringboot101.customer.CustomerRequest;
 import com.wbt.handsonspringboot101.customer.CustomerResponse;
 import org.junit.jupiter.api.Test;
@@ -9,6 +10,8 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
+
+import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,9 +26,10 @@ public class CustomerIntegrationTest {
     @Test
     void customerJourney() {
         // Forge new customer
-        final String name = "mansah";
-        final String email = "mansah@hotmail.com";
-        final int age = 45;
+        final var faker = new Faker();
+        final String name = faker.name().firstName();
+        final String email = name + "-test" + "@hotmail.com";
+        final int age = new Random().nextInt(13, 100);
         final var request = new CustomerRequest(name, email, age);
 
         // Save new customer
@@ -50,9 +54,26 @@ public class CustomerIntegrationTest {
                 .returnResult()
                 .getResponseBody();
         // Make sure a customer is present
-        final var expectedCustomer = new CustomerResponse(null, name, email, age); // because we don't the value of ID field
+        var expectedCustomer = new CustomerResponse(null, name, email, age); // because we don't the value of ID field
         assertThat(customers)
                 .usingRecursiveFieldByFieldElementComparatorIgnoringFields("id")
                 .contains(expectedCustomer);
+
+        // Get customer by Id
+        final var existingCustomer = customers.stream()
+                .filter(cr -> cr.email().equals(email))
+                .findFirst()
+                .orElseThrow();
+        expectedCustomer = new CustomerResponse(existingCustomer.id(), name, email, age);
+
+        webTestClient
+                .get()
+                .uri(URI.concat("/{id}"), existingCustomer.id())
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBody(new ParameterizedTypeReference<CustomerResponse>() {
+                })
+                .isEqualTo(expectedCustomer);
     }
 }
